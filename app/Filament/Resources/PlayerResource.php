523498efc846fce67f3ae03ae561e03e8e9126af
Sandro Domianidze <sdomianidze123@gmail.com>
2025-02-07 16:11:24 +0400

    feat: setup player form

--- app/Filament/Resources/PlayerResource.php
+++ app/Filament/Resources/PlayerResource.php
@@ -3,15 +3,13 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\PlayerResource\Pages;
-use App\Filament\Resources\PlayerResource\RelationManagers;
 use App\Models\Player;
 use Filament\Forms;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class PlayerResource extends Resource
 {
@@ -23,11 +21,15 @@ class PlayerResource extends Resource
     {
         return $form
             ->schema([
+                SpatieMediaLibraryFileUpload::make('avatar')
+                    ->collection('players')
+                    ->avatar()
+                    ->imageEditor()
+                    ->maxSize(1024 * 10)
+                    ->columnSpanFull(),
                 Forms\Components\TextInput::make('name')
                     ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('avatar')
-                    ->maxLength(255),
+                    ->maxLength(255)
             ]);
     }
 
